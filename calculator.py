f6b0fb976a6a390cd65063cdd8912c46cb8b0c8c
Vismaya Kalyan <vismayakalyan@gmail.com>
2020-06-07 20:18:09 -0500

    add read_json function

--- calculator.py
+++ calculator.py
@@ -1,16 +1,20 @@
 import numbers
 import sys
+import json
+
 
 class CalculatorError(Exception):
     """ Raise Exceptions """
 
+
 class Calculator:
     """ Calculator Methods """
+
     def add(self, a, b):
         self._check_operand(a)
         self._check_operand(b)
         return a + b
-       
+
     def subtract(self, a, b):
         self._check_operand(a)
         self._check_operand(b)
@@ -24,7 +28,7 @@ class Calculator:
     def divide(self, a, b):
         self._check_operand(a)
         self._check_operand(b)
-        try: 
+        try:
             return a / b
         except ZeroDivisionError:
             raise CalculatorError("Cant divide by zero.")
@@ -33,24 +37,34 @@ class Calculator:
         if not isinstance(operand, numbers.Number):
             raise CalculatorError(f'"{operand}" is not a number')
 
-if __name__ == '__main__':
+    def read_json(some_file_path):
+        with open(some_file_path, "r") as f:
+            return json.load(f)
+
+
+if __name__ == "__main__":
     print("Lets calculate")
     calculator = Calculator()
-    operations = [calculator.add, calculator.subtract, calculator.multiply, calculator.divide]
+    operations = [
+        calculator.add,
+        calculator.subtract,
+        calculator.multiply,
+        calculator.divide,
+    ]
     while True:
-        for i, operation in enumerate(operations,start =1):
-            print(f'{i}: {operation.__name__}')
-        print(f'q: Quit ')
+        for i, operation in enumerate(operations, start=1):
+            print(f"{i}: {operation.__name__}")
+        print(f"q: Quit ")
 
         operand = input("pick a operation: ")
-        if operand == 'q':
+        if operand == "q":
             sys.exit()
         op = int(operand)
         a = float(input("what is a? "))
         b = float(input("what is b? "))
-       
+
         try:
-            res = operations[op-1](a,b)
-            print(f' {a} {operations[op-1].__name__} {b} = {res}')
+            res = operations[op - 1](a, b)
+            print(f" {a} {operations[op-1].__name__} {b} = {res}")
         except CalculatorError as ex:
             print(ex)
